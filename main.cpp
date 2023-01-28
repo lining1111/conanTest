60c85e5ed2b2980e4173d381aea3207b53cda574
lining1111 <770677655@qq.com>
2023-01-28 17:02:30 +0800

    20230128

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "fmt/core.h"
 #include <opencv4/opencv2/opencv.hpp>
+#include<opencv2/core.hpp> /*包含库的核心功能*/
+#include<opencv2/imgproc.hpp> /*包括主要的图像处理函数*/
+#include<opencv2/highgui.hpp> /*提供了读写图像和视频的函数以及一些用户交互函数*/
 
 int execute_command(const std::string &command, std::string *output = nullptr,
                     bool redirect_stderr = false) {
@@ -23,9 +26,34 @@ int execute_command(const std::string &command, std::string *output = nullptr,
 }
 
 using namespace cv;
+Mat image_show(const char*image_path)
+{
+    //从文件中读入图像
+    Mat img = imread(image_path, 1);
+
+    //如果读入图像失败
+    if (img.empty())
+    {
+        std::cerr << "Can not load image:" << image_path << std::endl;
+        //fprintf(stderr, "Can not load image %s\n", image_path);
+        abort();//异常退出
+    }
+    //显示图像
+//    imshow("image", img);
+    std::cout << "image:" << img.rows << "," << img.cols << std::endl;
+    //此函数等待按键，按键盘任意键就返回
+//    waitKey(0);
+    /*官方解释是highgui
+    *没有给imshow绘制处理的时间。
+    *需要在imshow添加waitKey()
+    *waitKey()单位是ms*/
+    return img;
+}
+
 
 int main() {
     printf("%s\n", CV_VERSION);
+    image_show("./1.jpeg");
     std::string shell_command = {" /etc/passwd"};
 
     std::string output;
